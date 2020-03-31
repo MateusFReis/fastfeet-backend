c2c19bb943faf19207e713c1fdb4a2bd7c4942c1
Mateus Reis <ranmateus@gmail.com>
2020-03-30 21:42:36 -0300

    Entregadores adicionados

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,21 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
+import DeliverymanController from './app/controllers/DeliverymanController';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/sessions', SessionController.store);
+
+routes.post('/files', upload.single('file'), FileController.store);
+
+routes.get('/deliverymen', DeliverymanController.index);
+routes.post('/deliverymen', DeliverymanController.store);
+routes.put('/deliverymen/:id', DeliverymanController.update);
+routes.delete('/deliverymen/:id', DeliverymanController.delete);
 
 export default routes;
